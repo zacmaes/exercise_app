08c582730db5499c1e0df2d0f6bf957208478658
zacmaes <zachary.maes@gmail.com>
2022-12-02 15:49:51 -0600

    isBodyValid() func added and works for post requests, database posts work on mongodb

---
+++ exercise_rest/exercise_controller.mjs
@@ -0,0 +1,171 @@
+import 'dotenv/config';
+import * as exercises from './exercise_model.mjs';
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+/**
+*
+* @param {string} date
+* Return true if the date format is MM-DD-YY where MM, DD and YY are 2 digit integers
+*/
+function isDateValid(date) {
+    // Test using a regular expression. 
+    // To learn about regular expressions see Chapter 6 of the text book
+    const format = /^\d\d-\d\d-\d\d$/;
+    return format.test(date);
+}
+
+
+const isBodyValid = (name, reps, weight, unit, date) => {
+    if (name === undefined || reps === undefined || weight === undefined || unit === undefined || date === undefined){
+        console.log("missing property")
+        return false;
+    } else if (name === null || name.trim() === "") {
+        console.log("empty or null string name")
+        return false;
+    } else if (typeof reps !== "number" || reps <= 0) {
+        console.log("reps NaN or less than 0")
+        return false;
+    } else if (typeof weight !== "number" || weight <= 0) {
+        console.log("weight NaN or less than 0")
+        return false;
+    } else if (unit !== "kgs" && unit !== "lbs") {
+        console.log("unit not kgs or lbs")
+        return false;
+    } else if (!isDateValid(date)) {
+        console.log("invalid date")
+        return false;
+    }
+    console.log("EVERYTHING WORKS...")
+    return true;
+}
+
+/**
+ * Create a new exercise with the name, reps, weight, unit, and date provided in the body
+ */
+app.post('/exercises', asyncHandler(async (req, res) => {
+    if (isBodyValid(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)){
+        try {
+            const exercise = await exercises.createExercise(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date);
+            res.set('Content-Type', 'application/json');
+            res.status(201).json(exercise);
+            
+        } catch (error) {
+            // request error (from A7)
+            console.error(error);
+            res.status(400).json({ Error: 'Request Failed' });
+        }
+    } else {
+        // Body invalid if isBodyValid() returns False
+        res.set('Content-Type', 'application/json');
+        res.status(400).json({ Error: 'Invalid request' });
+    }
+    
+        
+    
+    // res.status(501).send({ Error: "Not implemented yet || app.post('/exercises'" });
+}));
+
+/**
+ * Retrieve All exercises. 
+ */
+ app.get('/exercises', asyncHandler(async (req, res) => {
+    try {
+        const filter = {};
+        const result = await exercises.findExercises(filter);
+        res.set('Content-Type', 'application/json');
+        res.status(200).json(result);
+        
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ Error: 'Request failed' });
+    }
+    // res.status(501).send({ Error: "Not implemented yet || app.get('/exercises'" });
+}));
+
+/**
+ * Retrive one exercise corresponding to the ID provided in the URL.
+ */
+app.get('/exercises/:_id', asyncHandler(async (req, res) => {
+    try {
+        const filter = { _id: req.params._id };
+        const result = await exercises.findExercises(filter);
+        if (result[0] !== null) {
+            res.set('Content-Type', 'application/json');
+            res.status(200).json(result[0]);
+        } else {
+            res.status(400).json({ Error: 'Resource Not Found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ Error: 'Request failed' });
+    }
+    // res.status(501).send({ Error: "Not implemented yet || app.get('/exercises/:_id'" });
+}));
+
+/**
+ * Update the exercise whose id is provided in the path parameter and set
+ * its name, reps, weight, unit, and date to the values provided in the body.
+ */
+app.put('/exercises/:_id', asyncHandler(async (req, res) => {
+    try {
+        const update = {
+            name: req.body.name,
+            reps: req.body.reps,
+            weight: req.body.weight,
+            unit: req.body.unit,
+            date: req.body.date
+        };
+        const resultVal = await exercises.updateExercises({_id: req.params._id}, update);
+
+        const returnObj = { _id: req.params._id };
+
+        if (resultVal > 0) {
+            returnObj.name = req.body.name;
+            returnObj.reps = req.body.reps;
+            returnObj.weight = req.body.weight;
+            returnObj.unit = req.body.unit;
+            returnObj.date = req.body.date;
+
+            res.set('Content-Type', 'application/json');
+            res.status(200).json(returnObj);
+        } else {
+            res.status(400).json({ Error: 'Nothing was updated' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ Error: 'Request failed' });
+    }
+    // res.status(501).send({ Error: "Not implemented yet || app.put('/exercises/:_id'" });
+}));
+
+/**
+ * Delete the exercise whose id is provided in the query parameters
+ */
+app.delete('/exercises/:_id', asyncHandler(async (req, res) => {
+    try {
+        const deleteConditions = { _id: req.params._id };
+        const deleteVal = await exercises.deleteExercises(deleteConditions);
+        if (deleteVal === 1) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ Error: 'Resource not found' });
+        }
+        
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ Error: 'Request failed' });
+    }
+    // res.status(501).send({ Error: "Not implemented yet || app.delete('/exercises/:_id'" });
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ exercise_rest/exercise_model.mjs
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+
+
+// Connect to to the database
+const db = mongoose.connection;
+// The open event is called when the database connection successfully opens
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+/**
+ * Define the schema
+ */
+const exerciseSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    reps: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    unit: { type: String, required: true },
+    date: { type: String, required: true }
+});
+
+/**
+ * Compile the model from the schema. This must be done after defining the schema.
+ */
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+/**
+ * Create a new exercise
+ * @param {String} name 
+ * @param {Number} reps 
+ * @param {Number} weight 
+ * @param {String} unit 
+ * @param {String} date 
+ * @returns A promise. Resolves to the JSON object for the document created by calling save().
+ */
+ const createExercise = async (name, reps, weight, unit, date) => {
+    const exercise = new Exercise({ name: name, reps: reps, weight: weight, unit: unit, date: date });
+    return exercise.save();
+}
+
+/**
+ * Find exercises that match the query parameter filter
+ * @param {Object} filter 
+ * @returns A promise. Resolves to and array of the JSON objects returned by calling .find() and .exec()
+ */
+ const findExercises = async (filter) => {
+    const query = Exercise.find(filter);
+    return query.exec();
+}
+
+/**
+ * Updates one exercise by their _id. Will update multiple key=value pairs if entered.
+ * @param {Object} filter 
+ * @param {Object} update 
+ * @returns A promise. Resolves to a number (either 1 or 0) representing the modified count
+ */
+ const updateExercises = async (filter, update) => {
+    const result = await Exercise.updateOne(filter, update);
+    return result.modifiedCount;
+}
+
+/**
+ * Deletes one or more exercises based upon the condition object passed as a parameter
+ * @param {Object} paramToDelete 
+ * @returns A promise. Resolves to a number showing the amount of exercises deleted
+ */
+ const deleteExercises = async (paramToDelete) => {
+    const result = await Exercise.deleteMany(paramToDelete);
+    return result.deletedCount;
+}
+
+export {createExercise, findExercises, updateExercises, deleteExercises};
